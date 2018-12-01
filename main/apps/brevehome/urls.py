49b113c0435af5fa7818b5673b84708618252ab5
Luke-Draper <lukepdraper@gmail.com>
2018-11-30 23:40:43 -0600

    views/urls setup checkpoint

--- main/apps/brevehome/urls.py
+++ main/apps/brevehome/urls.py
@@ -3,6 +3,16 @@ from . import views
 urlpatterns = [
 	url(r'^$',views.index),
 	url(r'^redirect$',views.redirect),
-	url(r'^post$',views.post)
+	url(r'^post$',views.post),
+
+
+
+	url(r'^test$',views.test_index),
+	url(r'^test/signup$',views.test_signup),
+	url(r'^test/login$',views.test_login),
+	url(r'^test/dashboard$',views.test_dashboard),
+	url(r'^test$',views.test_index),
+	url(r'^test$',views.test_index),
+	url(r'^test$',views.test_index),
 ]
 
--- main/apps/brevehome/views.py
+++ main/apps/brevehome/views.py
@@ -4,7 +4,7 @@ def index(request):
 		"dummy": "dummy"
 	}
 	request.session["dummy"] = "dummy"
-	return render(request,"brevehome/index.html",context)
+	return render(request,"brevehome/index.html")
 
 def redirect(request):
 	del request.session["dummy"]
@@ -21,5 +21,28 @@ def post(request):
 		return redirect("/")
 	else:
 		return redirect("/")
+
+
+def test_index(request):
+	check_session_login(request)
+	return render(request,"brevehome/index.html")
+
+def test_login(request):
+	check_session_login(request)
+	return render(request,"brevehome/login.html")
+
+def test_signup(request):
+	check_session_login(request)
+	return render(request,"brevehome/signup.html")
+
+def test_dashboard(request):
+	check_session_login(request)
+	return render(request,"brevehome/dashboard.html")
+
+
+def check_session_login(request):
+	if "test_logged_in" not in request.session:
+		request.session["test_logged_in"] = False
+
 
 
